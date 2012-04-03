b4a767727f0b40e23ecf57f54f05f752ab2d4c13
Chris Fordham <chris@xhost.com.au>
2012-04-03 11:15:43 +1000

    Add extra properties to metadata.

--- cookbooks/freeswitch/metadata.rb
+++ cookbooks/freeswitch/metadata.rb
@@ -1,6 +1,17 @@
 maintainer       "Chris Fordham"
-maintainer_email "chris@xhost.com.au"
+maintainer_email "chris.fordham@rightscale.com"
 license          "Apache 2.0"
 description      "Installs/Configures freeswitch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+supports "ubuntu"
+
+depends "apt"
+depends "build-essential"
+
+recipe "freeswitch:default", "Installs and configures Freeswitch from Apt."
+recipe "freeswitch::install", "Installs Freeswitch from Apt."
+recipe "freeswitch::configure", "Configures Freeswitch."
+recipe "freeswitch::install_from_package", "Installs Freeswitch by package from Apt."
+recipe "freeswitch::install_from_source", "Installs Freeswitch from source."
